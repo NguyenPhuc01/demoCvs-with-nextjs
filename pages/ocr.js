e9ed0f5e10db3f77a2f525ee16ca19f84543cc51
phucbndx <phucnguyendx9012@gmail.com>
2023-01-10 09:32:24 +0700

    update

--- pages/ocr.js
+++ pages/ocr.js
@@ -2,7 +2,8 @@ import { Button, Col, Divider, Row, Tabs } from "antd";
 import React, { memo, useState } from "react";
 import Resul from "../component/Resul";
 import UploadComponent from "../component/UploadComponent";
-const ocr = () => {
+const ocr = ({ resultConnect }) => {
+  console.log("🚀 ~ file: ocr.js:6 ~ ocr ~ resultConnect", resultConnect);
   const [result, setResult] = useState();
   const onChange = (key) => {
     console.log(key);
@@ -62,3 +63,29 @@ Tài liệu doanh nghiệp
 };
 
 export default memo(ocr);
+// export async function getServerSideProps(context) {
+//   var mysql = require("mysql2");
+
+//   var con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "123456",
+//   });
+
+//   con.connect(function (err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//   });
+//   const result = con.query(
+//     `INSERT INTO mydata.table1 (id, content)
+// VALUES (5, 'content 5')`,
+//     function (err, results, fields) {
+//       console.log(results); // results contains rows returned by server
+//       console.log(fields); // fields contains extra meta data about results, if available
+//     }
+//   );
+//   return {
+//     props: {
+
+//   }
+// }
